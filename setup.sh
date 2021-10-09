ca1624bfd0670794f79464d499adb4471c9ed96e
Dongliang Zhang <dzhang@mail.ccnu.edu.cn>
2021-10-09 21:41:12 +0800

    update the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #setup the scripts
+# echo `dirname $(realpath $0)`
+export SC_PATH=`dirname $(realpath $0)`
 
-export SC_PATH=
-
-### bin: ttl slideReport
+# ### bin: ttl slideReport
 export PATH=$SC_PATH/sh_scripts:$PATH
-
-### python path
+# 
+# ### python path
 export PYTHONPATH=$SC_PATH/py_scripts:$PYTHONPATH
-
-### ROOT macros: ttreelook, myPhi, savecanvas
-
+# 
+# ### ROOT macros: ttreelook, myPhi, savecanvas
+echo "add \$SC_PATH/root_scripts to your ~/.rootrc"
